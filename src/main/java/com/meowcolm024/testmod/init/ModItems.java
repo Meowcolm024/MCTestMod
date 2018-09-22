773ed34e75637b96af9868358ba1cfbe36442e4f
Meowcolm024 <luoyicong1026@live.com>
2018-09-22 16:09:15 +0800

    learning

--- src/main/java/com/meowcolm024/testmod/init/ModItems.java
+++ src/main/java/com/meowcolm024/testmod/init/ModItems.java
@@ -11,6 +11,7 @@ public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
     public static final Item RUBY = new ItemBase("ruby");
+    public static final Item PYTHON = new ItemBase("python");
 
 
 }
