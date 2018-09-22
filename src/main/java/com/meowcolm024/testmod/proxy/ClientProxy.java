fdeb567c01dd251bfcd99d8290e46ec3918a4414
Meowcolm024 <luoyicong1026@live.com>
2018-09-22 08:12:42 +0800

    Initial commit

---
+++ src/main/java/com/meowcolm024/testmod/proxy/ClientProxy.java
@@ -0,0 +1,4 @@
+package com.meowcolm024.testmod.proxy;
+
+public class ClientProxy extends CommonProxy {
+}
---
+++ src/main/java/com/meowcolm024/testmod/util/Reference.java
@@ -0,0 +1,11 @@
+package com.meowcolm024.testmod.util;
+
+public class Reference {
+    public static final String MOD_ID = "tm";
+    public static final String NAME = "Test Mod";
+    public static final String VERSION = "1.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "com.meowcolm024.testmod.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "com.meowcolm024.testmod.proxy.CommonProxy";
+
+}
